02be5629586f5f1d61f3309efd256d46e28b7ffc
Michiel Meeuwissen <git@meeuw.org>
2023-06-29 13:19:02 +0200

    Initial files (WIP)

---
+++ src/display.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>Value: {{ value }}</div>
+</template>
+
+<script>
+export default {
+	props: {
+		value: {
+			type: String,
+			default: '<mid>',
+		},
+	},
+  mounted() {
+    let pomsSelectorScript = document.createElement('script')
+    pomsSelectorScript.setAttribute('src', 'https://poms.omroep.nl/CMSSelector/media2.js')
+    document.head.appendChild(pomsSelectorScript)
+  },
+  methods: {
+    // methods of script
+  }
+};
+</script>
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import DisplayComponent from './display.vue';
+
+export default {
+	id: 'custom',
+	name: 'Custom',
+	icon: 'box',
+	description: 'poms selector',
+	component: DisplayComponent,
+	options: null,
+	types: ['string'],
+};
