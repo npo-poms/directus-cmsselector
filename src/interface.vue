48f8f91c059ffe4f6a77195fa98ddd1b0aa8c537
Michiel Meeuwissen <git@meeuw.org>
2023-09-07 23:46:09 +0200

    As long as it is not configurable, it must be production.

--- src/interface.vue
+++ src/interface.vue
@@ -37,7 +37,7 @@
       if (typeof nl_vpro_media_CMSSelector === "undefined") {
         let pomsSelectorScript = document.createElement('script')
         // TODO: This URL needs to be configurable
-        pomsSelectorScript.setAttribute('src', 'https://poms-acc.omroep.nl/CMSSelector/media2.js')
+        pomsSelectorScript.setAttribute('src', 'https://poms.omroep.nl/CMSSelector/media2.js')
         document.head.appendChild(pomsSelectorScript)
       }
       
